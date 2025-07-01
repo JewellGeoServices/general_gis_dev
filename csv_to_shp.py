8ccd25886c2c3182dfaa90e75d4f94039db269c2
SHJewell <scott.hjewell@gmail.com>
2025-07-01 15:22:46 -0700

    Updated csv_to_shp.py to save as single shp file, filter labels.

--- csv_to_shp.py
+++ csv_to_shp.py
@@ -17,16 +17,72 @@ def csv_to_shapefile(csv_file, shapefile_path):
     # Save the GeoDataFrame to a Shapefile
     gdf.to_file(shapefile_path, driver='ESRI Shapefile')
 
+def transform_coordinates(easting, northing):
+    # Assuming the input CRS is UTM Zone 33N (EPSG:32633)
+    input_crs = 'EPSG:32633'
+    output_crs = 'EPSG:4326'
+
+    transformer = pyproj.Transformer.from_crs(input_crs, output_crs)
+
+    # Transforming coordinates
+    return transformer.transform(easting, northing)
+
+def filter_out_origin_points(df, tolerance):
+
+    df['LONGITUDE'], df['LATITUDE'] = zip(*df.apply(lambda row: transform_coordinates(row['EASTING'], row['NORTHING']), axis=1))
+    mask = ~((abs(df['LONGITUDE']) < tolerance) & (abs(df['LATITUDE']) < tolerance))
+    filtered_df = df[mask]
+
+    return filtered_df
+
+def combine_to_shp(csv_folder, shapefile_path, label_filter=""):
+    """
+    Combine multiple CSV files into a single shapefile.
+
+    Parameters:
+    csv_files (list): List of paths to CSV files.
+    shapefile_path (str): Path to the output shapefile.
+    """
+
+    combined_dfs = []
+    csv_files = [os.path.join(csv_folder, file) for file in os.listdir(csv_folder) if file.endswith('.csv')]
+
+    # combined_df = pd.concat([pd.read_csv(file) for file in csv_files], ignore_index=True)
+    for file in csv_files:
+        df = pd.read_csv(file)
+        filtered = filter_out_origin_points(df, tolerance=0.00001)
+
+        if filtered.empty:
+            continue
+
+        filtered["LABEL"] = file.split(os.sep)[-1].replace(label_filter, "").replace(".csv", "")
+        combined_dfs.append(filtered)
+
+    combined_df = pd.concat(combined_dfs, ignore_index=True)
+
+    # filter_df = filter_out_origin_points(combined_df, tolerance=0.00001)
+    combined_df["geometry"] = [Point(xy) for xy in zip(combined_df['EASTING'], combined_df['NORTHING'])]
+
+    gdf = gpd.GeoDataFrame(combined_df, crs="EPSG:32629", geometry='geometry')
+
+    gdf.to_file(shapefile_path, driver='ESRI Shapefile')
+
 
 if __name__ == "__main__":
-    # Example usage
 
-    for file in os.listdir(r"Z:\Shared\ActiveProjects\25772 - Aoulouz Dam - Morocco - 2025\03 - Field Data\3- Culture etc\Aoulouz Dam - Morocco__25772 - Aoulouz Dam - Morocco__wst"):
-        if file.endswith(".csv"):
-            print(file)
-            csv_file_path = os.path.join(r"Z:\Shared\ActiveProjects\25772 - Aoulouz Dam - Morocco - 2025\03 - Field Data\3- Culture etc\Aoulouz Dam - Morocco__25772 - Aoulouz Dam - Morocco__wst", file)
-            shapefile_output_path = os.path.join(r"Z:\Shared\ActiveProjects\25772 - Aoulouz Dam - Morocco - 2025\06 - GIS\1 Vectors\2- Culture", file) + ".shp"
-            csv_to_shapefile(csv_file_path, shapefile_output_path)
+    source_folder = r"Z:\Shared\ActiveProjects\25772 - Aoulouz Dam - Morocco - 2025\03 - Field Data\3- Culture etc\Aoulouz Dam - Morocco__25772 - Aoulouz Dam - Morocco__wst"
+    write_file = r"Z:\Shared\ActiveProjects\25772 - Aoulouz Dam - Morocco - 2025\06 - GIS\1 Vectors\2- Culture\CULTURE MEGAFILE.shp"
+    label_filter = "Culture_no-device_ncastioni_2025-06-17_"
+
+    combine_to_shp(source_folder, write_file, label_filter=label_filter)
+
+    # for file in os.listdir(r"Z:\Shared\ActiveProjects\25772 - Aoulouz Dam - Morocco - 2025\03 - Field Data\3- Culture etc\Aoulouz Dam - Morocco__25772 - Aoulouz Dam - Morocco__wst"):
+    #     if file.endswith(".csv"):
+    #         print(file)
+    #         csv_file_path = os.path.join(r"Z:\Shared\ActiveProjects\25772 - Aoulouz Dam - Morocco - 2025\03 - Field Data\3- Culture etc\Aoulouz Dam - Morocco__25772 - Aoulouz Dam - Morocco__wst", file)
+    #         file_name = file.replace(".csv", ".shp")
+    #         shapefile_output_path = os.path.join(r"Z:\Shared\ActiveProjects\25772 - Aoulouz Dam - Morocco - 2025\06 - GIS\1 Vectors\2- Culture", file_name)
+    #         csv_to_shapefile(csv_file_path, shapefile_output_path)
 
     # csv_file_path = r"Z:\Shared\ActiveProjects\25772 - Aoulouz Dam - Morocco - 2025\03 - Field Data\3- Culture etc\Aoulouz Dam - Morocco__25772 - Aoulouz Dam - Morocco__wst\Culture_no-device_ncastioni_2025-06-17_ dwB- cement begins here- steps start on this line.csv"  # Update this with your CSV file path
     # shapefile_output_path = r'Culture_no-device_ncastioni_2025-06-17_ dwB- cement begins here- steps start on this line.csv.shp'  # Output shapefile path
